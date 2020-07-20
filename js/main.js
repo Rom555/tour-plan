99cdc0d33931b4e57ba6258080404f9f85502921
Rom555 <romanzubrilin@gmail.com>
2020-07-20 19:44:56 +0300

    create: keyboard slider control, slider's images

--- js/main.js
+++ js/main.js
@@ -2,19 +2,13 @@ var mySwiper = new Swiper(".swiper-container", {
   // Optional parameters
   loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
-
   // Navigation arrows
   navigation: {
     nextEl: ".slider-button--next",
     prevEl: ".slider-button--prev",
   },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
+  keyboard: {
+    enabled: true,
   },
 });
